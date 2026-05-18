9c128b0da0404aca1078aa8631696fc45007e428
Etty <etty70910@gmail.com>
2026-05-18 12:15:49 +0300

    feat: logout button

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../auth/auth-service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.scss',
 })
 export class Header {
-
+  constructor(private _authService: AuthService, private _router: Router) {}
+  logout() {
+    localStorage.removeItem('token');
+    this._authService.currentUser.set(null);
+    this._router.navigate(['/login']);
+  }
 }
